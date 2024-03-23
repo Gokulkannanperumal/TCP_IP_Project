ddf3bb8ff27d1fff696014436f9623151b7d541f
Gokulkannan perumal <kanna95be@gmail.com>
2024-03-23 18:20:38 +0530

    Add project files.

---
+++ TCP.Client/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Set the server's IP address and port
+                string serverIp = "192.168.1.4";
+                int port = 12345;
+
+                // Create a TCP/IP socket
+                TcpClient client = new TcpClient(serverIp, port);
+
+                // Get a stream object for reading and writing
+                NetworkStream stream = client.GetStream();
+
+                // Prompt the user for input
+                Console.Write("Enter a message to send: ");
+                string message = Console.ReadLine();
+
+                // Convert the message to bytes
+                byte[] data = Encoding.ASCII.GetBytes(message);
+
+                // Send the data to the server
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                // Receive the response from the server
+                data = new byte[2048];
+                int bytesRead = stream.Read(data, 0, data.Length);
+                string response = Encoding.ASCII.GetString(data, 0, bytesRead);
+
+                Console.WriteLine("Received: {0}", response);
+                Console.ReadKey();
+                // Close the connection
+                client.Close();
+
+               
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ TCP.Server/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP.Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+            try
+            {
+                // Set the server's IP address and port
+                IPAddress ipAddress = IPAddress.Parse("192.168.1.4");
+                int port = 12345;
+
+                // Create a TCP/IP socket
+                server = new TcpListener(ipAddress, port);
+
+                // Start listening for client connections
+                server.Start();
+
+                Console.WriteLine("Server is listening on port 12345");
+
+                // Enter the listening loop
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+
+                    // Accept the client connection
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine($"Connection from {((IPEndPoint)client.Client.RemoteEndPoint).Address}");
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    // Buffer for the data
+                    byte[] bytes = new byte[2048];
+                    int bytesRead;
+
+                    // Loop to receive all the data sent by the client
+                    while ((bytesRead = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Convert the data bytes to a string
+                        string data = Encoding.ASCII.GetString(bytes, 0, bytesRead);
+                        Console.WriteLine($"Received: {data}");
+
+                        // Echo the data back to the client
+                        byte[] response = Encoding.ASCII.GetBytes(data);
+                        stream.Write(response, 0, response.Length);
+                    }
+
+                    // Close the connection
+                    client.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                // Stop listening for new clients
+                server?.Stop();
+            }
+        }
+    }
+}
